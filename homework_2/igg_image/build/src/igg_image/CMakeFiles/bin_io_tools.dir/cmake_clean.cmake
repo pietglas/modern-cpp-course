250ed8ec2f493b969e268e0976eed6d24c1128ea
piet@piet-Latitude-E7440 <pietglas77@gmail.com>
2020-03-20 00:17:36 +0100

    removed build files

--- homework_2/igg_image/build/src/igg_image/CMakeFiles/bin_io_tools.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bin_io_tools.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- homework_2/igg_image/build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/piet/Projects/modern-cpp-course/homework_2/igg_image/tests
-# Build directory: /home/piet/Projects/modern-cpp-course/homework_2/igg_image/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_image "tests/test_image")
-subdirs("../gtest")
