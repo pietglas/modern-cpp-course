6b1655bb05d13ca5ce6d3ad9b58a0247a16ad1d6
piet@piet-Latitude-E7440 <pietglas77@gmail.com>
2020-03-23 00:34:13 +0100

    removed build files

--- homework_3/igg_image/build/gtest/CMakeFiles/gtest_main.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/piet/Projects/modern-cpp-course/homework_3/igg_image/lib/libgtest_main.a"
-)
--- homework_3/igg_image/build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/piet/Projects/modern-cpp-course/homework_3/igg_image/tests
-# Build directory: /home/piet/Projects/modern-cpp-course/homework_3/igg_image/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_image "/home/piet/Projects/modern-cpp-course/homework_3/igg_image/bin/test_image")
-subdirs("../gtest")
